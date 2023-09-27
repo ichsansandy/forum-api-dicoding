c50e0d53025788a75546b454b66cff8a281a178c
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-27 23:03:32 +0700

    add ssl true

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -7,8 +7,9 @@ const testConfig = {
   user: process.env.PGUSER_TEST,
   password: process.env.PGPASSWORD_TEST,
   database: process.env.PGDATABASE_TEST,
+  ssl: true
 };
 
-const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool();
+const pool = process.env.NODE_ENV === 'test' ? new Pool(testConfig) : new Pool({ssl: true});
 
 module.exports = pool;
